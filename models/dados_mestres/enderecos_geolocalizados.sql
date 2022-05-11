4ea77b9c0a1a7e35bdd53b452e4947edf91b8739
patriciacatandi <patriciabcatandi@gmail.com>
2022-05-11 17:56:46 -0300

    particionando enderecos_geolocalizados

--- models/dados_mestres/enderecos_geolocalizados.sql
+++ models/dados_mestres/enderecos_geolocalizados.sql
@@ -1,3 +1,14 @@
+{{
+    config(
+        materialized='incremental',
+        partition_by={
+            "field": "data_particao",
+            "data_type": "date",
+            "granularity": "month",
+        }
+    )
+}}
+
 SELECT
     SAFE_CAST(endereco_completo AS STRING) endereco_completo,
     SAFE_CAST(pais AS STRING) pais,
@@ -9,5 +20,13 @@ SELECT
     SAFE_CAST(SAFE_CAST(numero_porta AS FLOAT64) AS INT64) numero_porta,
     SAFE_CAST(latitude AS FLOAT64) latitude,
     SAFE_CAST(longitude AS FLOAT64) longitude,
-    ST_GEOGPOINT(SAFE_CAST(longitude AS FLOAT64), SAFE_CAST(latitude AS FLOAT64)) AS geometry
-FROM rj-escritorio-dev.dados_mestres_staging.enderecos_geolocalizados AS t
+    ST_GEOGPOINT(SAFE_CAST(longitude AS FLOAT64), SAFE_CAST(latitude AS FLOAT64)) AS geometry,
+    SAFE_CAST(DATE_TRUNC(DATE(data_particao), month) AS DATE) data_particao,
+FROM rj-escritorio-dev.dados_mestres_staging.enderecos_geolocalizados_temp AS t
+
+
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE data_particao > (SELECT max(data_particao) FROM {{ this }})
+
+{% endif %}
