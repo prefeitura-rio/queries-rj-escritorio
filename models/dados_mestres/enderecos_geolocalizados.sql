ee668678dd0a630d103ee73e9b5f1706bed6aafd
patriciacatandi <patriciabcatandi@gmail.com>
2022-05-11 14:16:11 -0300

    alteração no cast

--- models/dados_mestres/enderecos_geolocalizados.sql
+++ models/dados_mestres/enderecos_geolocalizados.sql
@@ -6,7 +6,7 @@ SELECT
     SAFE_CAST(bairro AS STRING) bairro,
     LPAD(SAFE_CAST(REGEXP_REPLACE(id_logradouro, r'\.0$', '') AS STRING), 6, '0') id_logradouro,
     SAFE_CAST(logradouro AS STRING) logradouro,
-    SAFE_CAST(numero_porta AS INT64) numero_porta,
+    SAFE_CAST(SAFE_CAST(numero_porta AS FLOAT64) AS INT64) numero_porta,
     SAFE_CAST(latitude AS FLOAT64) latitude,
     SAFE_CAST(longitude AS FLOAT64) longitude,
     ST_GEOGPOINT(SAFE_CAST(longitude AS FLOAT64), SAFE_CAST(latitude AS FLOAT64)) AS geometry
