f7305d072f75e57d98015a2d15b65a150897aa25
João Carabetta <joao.carabetta@gmail.com>
2022-09-08 18:29:16 -0300

    Update bairro.sql

--- models/dados_mestres/bairro.sql
+++ models/dados_mestres/bairro.sql
@@ -1,3 +1,5 @@
+
+WITH t as (
 SELECT 
     SAFE_CAST(REGEXP_REPLACE(LTRIM(CODBAIRRO ,'0') , r'\.0$', '') AS STRING) id_bairro,
     SAFE_CAST(RTRIM(NOME) AS STRING) nome,
@@ -9,4 +11,19 @@ SELECT
     SAFE_CAST(Area AS FLOAT64) area,
     SAFE_CAST(SHAPESTLength AS FLOAT64) perimetro, 
     SAFE_CAST(geometry AS STRING) geometry, # TODO, resolver id_bairro = '004' e converter para GEOGRAPHY
-FROM `rj-escritorio-dev.dados_mestres_staging.bairro`
+FROM `rj-escritorio-dev.dados_mestres_staging.bairro`)
+SELECT 
+  id_bairro,
+  t.nome,
+  t.id_area_planejamento,
+  t.id_regiao_planejamento,
+  t.nome_regiao_planejamento,
+  t.id_regiao_administrativa,
+  t.nome_regiao_administrativa,
+  t2.nome subprefeitura,
+  area,
+  perimetro,
+  geometry
+FROM t
+LEFT JOIN `rj-escritorio-dev.dados_mestres.subprefeituras_regiao_adm` t2
+ON t.id_regiao_administrativa = cast(t2.id_regiao_administrativa as string)
