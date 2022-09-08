dfd9416298b3665753e26b28c3943354b28768ff
João Carabetta <joao.carabetta@gmail.com>
2022-09-08 17:57:48 -0300

    Update bairro.sql

--- models/dados_mestres/bairro.sql
+++ models/dados_mestres/bairro.sql
@@ -5,7 +5,7 @@ SELECT
     SAFE_CAST(REGEXP_REPLACE(LTRIM(Cod_RP ,'0'), r'\.0$', '') AS STRING) id_regiao_planejamento,
     SAFE_CAST(RP AS STRING) nome_regiao_planejamento,
     SAFE_CAST(REGEXP_REPLACE(LTRIM(CODRA ,'0'), r'\.0$', '') AS STRING) id_regiao_administrativa,
-    SAFE_CAST(RTRIM(REGIAO_ADM) AS STRING) nome_regiao_administrativa,
+    SAFE_CAST(INITCAP(RTRIM(REGIAO_ADM)) AS STRING) nome_regiao_administrativa,
     SAFE_CAST(Area AS FLOAT64) area,
     SAFE_CAST(SHAPESTLength AS FLOAT64) perimetro, 
     SAFE_CAST(geometry AS STRING) geometry, # TODO, resolver id_bairro = '004' e converter para GEOGRAPHY
