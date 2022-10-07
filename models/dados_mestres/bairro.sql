fd69c438d8c0084d35a7f4446c05c59b71847342
d116626 <d116626@gmail.com>
2022-10-07 14:02:18 -0300

    fix: change col names

--- models/dados_mestres/bairro.sql
+++ models/dados_mestres/bairro.sql
@@ -1,13 +1,13 @@
 SELECT 
-    SAFE_CAST(REGEXP_REPLACE(LTRIM(CODBAIRRO ,'0') , r'\.0$', '') AS STRING) id_bairro,
-    SAFE_CAST(NOME AS STRING) nome,
-    SAFE_CAST(REGEXP_REPLACE(LTRIM(AREA_PLANE ,'0'), r'\.0$', '') AS STRING) id_area_planejamento,
-    SAFE_CAST(REGEXP_REPLACE(LTRIM(Cod_RP ,'0'), r'\.0$', '') AS STRING) id_regiao_planejamento,
-    SAFE_CAST(RP AS STRING) nome_regiao_planejamento,
-    SAFE_CAST(REGEXP_REPLACE(LTRIM(CODRA ,'0'), r'\.0$', '') AS STRING) id_regiao_administrativa,
-    SAFE_CAST(REGIAO_ADM AS STRING) nome_regiao_administrativa,
-    SAFE_CAST(Area AS FLOAT64) area,
-    SAFE_CAST(SHAPESTLength AS FLOAT64) perimetro, 
+    SAFE_CAST(REGEXP_REPLACE(LTRIM(codbairro ,'0') , r'\.0$', '') AS STRING) id_bairro,
+    SAFE_CAST(nome AS STRING) nome,
+    SAFE_CAST(REGEXP_REPLACE(LTRIM(area_plane ,'0'), r'\.0$', '') AS STRING) id_area_planejamento,
+    SAFE_CAST(REGEXP_REPLACE(LTRIM(cod_rp ,'0'), r'\.0$', '') AS STRING) id_regiao_planejamento,
+    SAFE_CAST(rp AS STRING) nome_regiao_planejamento,
+    SAFE_CAST(REGEXP_REPLACE(LTRIM(codra ,'0'), r'\.0$', '') AS STRING) id_regiao_administrativa,
+    SAFE_CAST(regiao_adm AS STRING) nome_regiao_administrativa,
+    SAFE_CAST(area AS FLOAT64) area,
+    SAFE_CAST(shapestlength AS FLOAT64) perimetro, 
     SAFE_CAST(geometry AS STRING) geometry, # TODO, resolver id_bairro = '004' e converter para GEOGRAPHY
 FROM `rj-escritorio-dev.dados_mestres_staging.bairro`
 
