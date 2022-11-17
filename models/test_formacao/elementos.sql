b10d2626ef1d3b152635baa13c22eee5c7283f5a
Bruno Almeida <brunodepauloalmeida@gmail.com>
2022-11-17 18:52:27 -0300

    add: case when in paises_americanos and remove for from elementos

--- models/test_formacao/elementos.sql
+++ models/test_formacao/elementos.sql
@@ -2,8 +2,5 @@ SELECT
   SAFE_CAST(data as DATE) as data,
   SAFE_CAST(number as INT64) as numero,
   element as elemento,
-   {% for ELEMENTO in ELEMENTOS_LISTA %}
-    CASE WHEN element = '{{ELEMENTO}}' then TRUE ELSE FALSE end) as indicador_{{ELEMENTO}},
-   {% endfor %}
 FROM `rj-escritorio-dev.test_formacao_staging.test_table_2`
 WHERE data < "{{var('ELEMENTOS_DATA_INICIAL')}}"
