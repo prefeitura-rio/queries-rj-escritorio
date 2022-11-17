a8e0b3cc84eb4755592a298e72336749841f2ace
Bruno Almeida <brunodepauloalmeida@gmail.com>
2022-11-17 16:47:49 -0300

    add dbt example

---
+++ models/test_formacao/paises_americanos.sql
@@ -0,0 +1,11 @@
+SELECT
+  LPAD(id_pais, 2, '0') as id_pais,
+  CASE 
+    WHEN continente = 'América do Sul' THEN '01'
+    WHEN continente = 'América Central' THEN '02'
+    WHEN continente = 'América do Norte' THEN '03'
+    ELSE '00'
+  END as id_continente,
+  pais,
+  capital
+FROM `rj-escritorio-dev.test_formacao_staging.test_table`
