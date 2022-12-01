e10f01d07f8d963485b6255fdfdf8cb6c4ebeea8
d116626 <d116626@gmail.com>
2022-12-01 18:51:51 -0300

    feat: add zoneamento and edificacoes models

--- models/dados_mestres/edificacoes.sql
+++ models/dados_mestres/edificacoes.sql
@@ -1,3 +1,14 @@
-SELECT
-    *
-FROM `rj-escritorio-dev.dados_mestres_staging.edificacoes`
+SELECT 
+    SAFE_CAST(TRIM(fid) AS STRING) AS id,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(cod_unico), r'\.0$', '') AS STRING) AS id_unico,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(cod_projec), r'\.0$', '') AS STRING) AS id_projecao,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(cod_lote), r'\.0$', '') AS STRING) AS id_lote,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(clnp), r'\.0$', '') AS STRING) AS id_logradouro_numero_porta,
+    SAFE_CAST(TRIM(tipo) AS STRING) AS tipo,
+    SAFE_CAST(REGEXP_REPLACE(topo, r',', '.') AS FLOAT64) AS altitude_topo,
+    SAFE_CAST(REGEXP_REPLACE(base, r',', '.') AS FLOAT64) AS altitude_base,
+    SAFE_CAST(REGEXP_REPLACE(altura, r',', '.') AS FLOAT64) AS altura,
+    SAFE_CAST(REGEXP_REPLACE(shape__area, r',', '.') AS FLOAT64) AS area,
+    SAFE_CAST(REGEXP_REPLACE(shape__length, r',', '.') AS FLOAT64) AS comprimento,
+    SAFE_CAST(geometry AS STRING) AS geometry_wkt, #TODO: CONVERT TO GEOGRAPHY
+FROM rj-escritorio-dev.dados_mestres_staging.edificacoes AS t
--- models/dados_mestres/zoneamento_subzonas_subsetores.sql
+++ models/dados_mestres/zoneamento_subzonas_subsetores.sql
@@ -1,3 +1,12 @@
-SELECT
-    *
-FROM `rj-escritorio-dev.dados_mestres_staging.zoneamento_subzonas_subsetores`
+SELECT 
+    SAFE_CAST(TRIM(sigla) AS STRING) AS sigla,
+    SAFE_CAST(TRIM(nome) AS STRING) AS nome,
+    SAFE_CAST(TRIM(legislacao) AS STRING) AS legislacao,
+    SAFE_CAST(TRIM(abrev) AS STRING) AS abreviacao,
+    SAFE_CAST(TRIM(legislacao_extenso) AS STRING) AS legislacao_extenso,
+    SAFE_CAST(REGEXP_REPLACE(st_areashape, r',', '.') AS FLOAT64) AS area,
+    SAFE_CAST(REGEXP_REPLACE(st_perimetershape, r',', '.') AS FLOAT64) AS perimetro,
+    SAFE_CAST(TRIM(leg) AS STRING) AS legenda_abreviada,
+    SAFE_CAST(TRIM(legenda) AS STRING) AS legenda,
+    ST_GEOGFROMTEXT(geometry) AS geometry,
+FROM rj-escritorio-dev.dados_mestres_staging.zoneamento_subzonas_subsetores AS t
