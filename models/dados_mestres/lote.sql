af37f047a898b5837a9544900324c8538f2be461
d116626 <d116626@gmail.com>
2022-12-20 18:37:11 -0300

    feat: add geometry_wkt

--- models/dados_mestres/lote.sql
+++ models/dados_mestres/lote.sql
@@ -1,2 +1,8 @@
-SELECT * 
+SELECT 
+    * except(
+        geometria,
+        geometry
+    ) 
+    SAFE_CAST(geometry_wkt AS STRING) AS geometry_wkt, 
+    SAFE.ST_GEOGFROMTEXT(geometry) AS geometry, #TODO: CONVERT TO GEOGRAPHY
 FROM `rj-escritorio-dev.dados_mestres_staging.lote` 
