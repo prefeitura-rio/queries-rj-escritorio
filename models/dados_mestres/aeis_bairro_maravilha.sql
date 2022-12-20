deffdc7f734a0ed17245ac9bb5df39c2b5e0da50
d116626 <d116626@gmail.com>
2022-12-20 18:47:06 -0300

    feat: add geometry_wkt

--- models/dados_mestres/aeis_bairro_maravilha.sql
+++ models/dados_mestres/aeis_bairro_maravilha.sql
@@ -1,8 +1,8 @@
 SELECT
     * except(
-        geometria,
-        geometry
-    ) 
+        geometry,
+        geometry_wkt
+    ),
     SAFE_CAST(geometry_wkt AS STRING) AS geometry_wkt, 
     SAFE.ST_GEOGFROMTEXT(geometry) AS geometry, #TODO: CONVERT TO GEOGRAPHY
 FROM `rj-escritorio-dev.dados_mestres_staging.aeis_bairro_maravilha` 
