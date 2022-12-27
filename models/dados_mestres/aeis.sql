853f97e92e4689874b46504b3559d8442bcf929b
Replit user <>
2022-12-27 18:04:18 +0000

    chores: reorder and remove null variables

--- models/dados_mestres/aeis.sql
+++ models/dados_mestres/aeis.sql
@@ -1,27 +1,25 @@
 
 SELECT 
-    SAFE_CAST(objectid AS STRING) id_object,
-    SAFE_CAST(cod AS STRING) id_aeis, # 309 ids únicos de 996 entradas (aprox. 31% das entradas)
-    SAFE_CAST(item AS STRING) item,
-    SAFE_CAST(planta_cad AS STRING) planta_cadastral,
-    SAFE_CAST(nome_do_ar AS STRING) nome_ar, # verificar metadado
-    SAFE_CAST(f6 AS STRING) referencia, # verificar metadado
-    SAFE_CAST(SAFE.PARSE_DATE('%d/%m/%Y', data) AS DATE) data_cadastro,  
-    SAFE_CAST(ra AS STRING) nome_regiao_administrativa,
-    SAFE_CAST(pal AS STRING) projeto_loteamento, # boolean
-    SAFE_CAST(tipologia AS STRING) tipologia,
+    SAFE_CAST(cod AS STRING) id_aeis, 
     SAFE_CAST(nome AS STRING) nome_aeis,
-    SAFE_CAST(legislacao AS STRING) legislacao,
+    SAFE_CAST(tipologia AS STRING) tipologia,
+    SAFE_CAST(item AS STRING) item,
     SAFE_CAST(setor AS STRING) nome_setor,
     SAFE_CAST(sigla AS STRING) nome_sigla,
-    SAFE_CAST(pavimentos AS STRING) limite_pavimentos_permitidos, # verificar metadado
+    SAFE_CAST(nome_do_ar AS STRING) nome_ar, # verificar metadado
+    SAFE_CAST(planta_cad AS STRING) planta_cadastral,
+    SAFE_CAST(ra AS STRING) nome_regiao_administrativa,
+    SAFE_CAST(pal AS STRING) projeto_loteamento, # boolean
+    SAFE_CAST(f6 AS STRING) referencia, # verificar metadado
+    SAFE_CAST(SAFE.PARSE_DATE('%d/%m/%Y', data) AS DATE) data_cadastro,  
+    SAFE_CAST(legislacao AS STRING) legislacao,
+    SAFE_CAST(pavimentos AS STRING) limite_pavimentos_permitido,
     SAFE_CAST(tx_ocupacao AS STRING) taxa_ocupacao,
     SAFE_CAST(REGEXP_REPLACE(iat, r',', '.') AS FLOAT64) AS indice_aproveitamento_terreno,
     SAFE_CAST(coef_adensamento AS STRING) coeficiente_adensamento,
     SAFE_CAST(afastamentos AS STRING) afastamento,
-    SAFE_CAST(shape__area AS STRING) area_poligono,
-    SAFE_CAST(shape__length AS STRING) comprimento_poligono,
-
+    SAFE_CAST(shape__area AS STRING) area,
+    SAFE_CAST(shape__length AS STRING) comprimento,
     SAFE_CAST(geometry_wkt AS STRING) geometria_wkt, 
     SAFE.ST_GEOGFROMTEXT(geometry) AS geometry, 
 FROM `rj-escritorio-dev.dados_mestres.aeis`
--- models/dados_mestres/lote.sql
+++ models/dados_mestres/lote.sql
@@ -1,12 +1,10 @@
 
 SELECT 
-    SAFE_CAST(objectid_12 AS STRING) id_object, 
-    SAFE_CAST(objectid_1 AS STRING) id_object_1, 
-    SAFE_CAST(cod_lote AS STRING) id_lote, # 883298 ids únicos de 999757 entradas
-    SAFE_CAST(obs AS STRING) observacao,
+    SAFE_CAST(cod_lote AS STRING) id_lote, 
     SAFE_CAST(cod_quadra AS STRING) id_quadra, 
     SAFE_CAST(cod_np AS STRING) id_numero_porta, 
     SAFE_CAST(cod_trecho AS STRING) id_trecho,  
+    SAFE_CAST(obs AS STRING) observacao,
     SAFE_CAST(shape__area AS STRING) area_poligono, 
     SAFE_CAST(shape__length AS STRING) comprimento_poligono, 
     SAFE_CAST(geometry_wkt AS STRING) geometria_wkt, 
